c063d26ce834d24ce4c9b0c208760f0489728eb0
Vince Prignano <vincepri@vmware.com>
2021-09-20 07:35:52 -0700

    Add v1beta1 bootstrap kubeadm types
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

--- bootstrap/kubeadm/api/v1beta1/doc.go
+++ bootstrap/kubeadm/api/v1beta1/doc.go
@@ -14,6 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha4 contains the v1alpha4 API implementation.
-// +k8s:conversion-gen=sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1
-package v1alpha4
+package v1beta1
